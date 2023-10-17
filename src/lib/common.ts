9b302d88b3c7eddef85964301a7925ee9a50ba3f
7i7o <mparij@gmail.com>
2023-10-17 18:45:07 -0300

    add colors to console messages

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -4,19 +4,37 @@ import { execSync } from 'child_process';
 import { readFileSync } from 'fs';
 import type { JsonWebKey } from 'crypto';
 
+const ANSI_RESET = '\x1b[0m';
+const ANSI_RED = '\x1b[31m';
+const ANSI_GREEN = '\x1b[32m';
 export const PL_TMP_PATH = '.protocol.land';
 export const GIT_CONFIG_KEYFILE = 'protocol.land.keyfile';
 export const getWarpContractTxId = () =>
     'w5ZU15Y2cLzZlu3jewauIlnzbKw-OAxbN9G5TbuuiDQ';
 
-export const log = (message: any) => console.error(` [PL] ${message}`);
+export const log = (message: any, options?: { color: 'red' | 'green' }) => {
+    if (!options) console.error(` [PL] ${message}`);
+    else {
+        const { color } = options;
+        console.error(
+            `${
+                color === 'red' ? ANSI_RED : ANSI_GREEN
+            } [PL] ${message}${ANSI_RESET}`
+        );
+    }
+};
 
 let wallet: JsonWebKey | null = null;
 export const getWallet = () => {
     const walletNotFound = () => {
         log(`Failed to get wallet keyfile path from git config.`);
         log(
-            `Run 'git config --add ${GIT_CONFIG_KEYFILE} YOUR_WALLET_KEYFILE_FULL_PATH' to set up`
+            `You need an owner or contributor wallet to have write access to the repo.`,
+            { color: 'red' }
+        );
+        log(
+            `Run 'git config --add ${GIT_CONFIG_KEYFILE} YOUR_WALLET_KEYFILE_FULL_PATH' to set it up`,
+            { color: 'green' }
         );
         return null;
     };

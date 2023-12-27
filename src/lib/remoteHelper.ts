866e18d5c9311239b689c5ed1120845d9dc2943e
7i7o <mparij@gmail.com>
2023-12-27 15:52:53 -0300

    PL-140 add todo for amount limit #comment testing integration

--- src/lib/remoteHelper.ts
+++ src/lib/remoteHelper.ts
@@ -199,6 +199,7 @@ const spawnPipedGitCommand = async (
             waitFor(1000);
 
             // TODO: show TX info and wait for user confirmation if not subsidized
+            // TODO: add git config key to set a limit amount to ask for confirmation
 
             const success = await uploadProtocolLandRepo(
                 pathToPack,
